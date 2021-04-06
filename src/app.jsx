3690ad836d1b0322cab718d991f5622bd8fc5c0a
ByungyeonKim <viva_kby@naver.com>
2021-04-06 23:52:25 +0900

    README.md update

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './app.css';
 
 function App() {
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&regionCode=KR&maxResults=25&key=${apiKey}`
+      , requestOptions
+    )
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+
   return <h1> Hello :)</h1>;
 }
 
