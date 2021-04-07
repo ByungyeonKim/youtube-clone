da9bb14e7e6fff1035428d88f7285553aec46719
ByungyeonKim <viva_kby@naver.com>
2021-04-07 17:05:19 +0900

    video list, item 생성

---
+++ src/ components/video_item/video_item.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const VideoItem = (props) => (
+            
+);
+
+export default VideoItem;
---
+++ src/ components/video_list/video_list.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const VideoList = (props) => (
+            
+);
+
+export default VideoList;
