0f4a389d598da881688332455b76c2c6dbc8a383
ByungyeonKim <viva_kby@naver.com>
2021-04-08 23:36:31 +0900

    VideoItem 컴포넌트에 key 추가

--- src/ components/video_list/video_list.jsx
+++ src/ components/video_list/video_list.jsx
@@ -4,7 +4,7 @@ import VideoItem from '../video_item/video_item';
 const VideoList = (props) => (
   <ul>
     {props.videos.map((video) => (
-      <VideoItem video={video} />
+      <VideoItem key={video.id} video={video} />
     ))}
   </ul>
 );
