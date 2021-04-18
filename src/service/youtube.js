251f68bd191d29e8461844b8e40a70a684336305
ByungyeonKim <viva_kby@naver.com>
2021-04-18 14:49:14 +0900

    네트워크 통신 분리

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -6,25 +6,30 @@ class Youtube {
       redirect: 'follow',
     };
   }
-  mostPopular() {
-    return fetch(
-      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&regionCode=KR&maxResults=24&key=${this.key}`,
-      this.getRequestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => result.items)
-      .catch((error) => console.log('error', error));
+  async mostPopular() {
+    try {
+      const response = await fetch(
+        `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&regionCode=KR&maxResults=24&key=${this.key}`,
+        this.getRequestOptions
+      );
+      const result = await response.json();
+      return result.items;
+    } catch (error) {
+      return console.log('error', error);
+    }
   }
 
-  search(query) {
-    return fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=24&q=${query}&type=video&key=${this.key}`,
-      this.getRequestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => result.items.map((item) => ({ ...item, id: item.id.videoId })))
-      .then((items) => items)
-      .catch((error) => console.log('error', error));
+  async search(query) {
+    try {
+      const response = await fetch(
+        `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=24&q=${query}&type=video&key=${this.key}`,
+        this.getRequestOptions
+      );
+      const result = await response.json();
+      return result.items.map((item) => ({ ...item, id: item.id.videoId }));
+    } catch (error) {
+      return console.log('error', error);
+    }
   }
 }
 
