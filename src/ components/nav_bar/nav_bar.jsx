7bf2908cf20fc6f3d5cdaf1db2a46ecfe1650a51
ByungyeonKim <viva_kby@naver.com>
2021-04-30 16:20:47 +0900

    블로그 navbar 메뉴 추가

--- src/ components/nav_bar/nav_bar.jsx
+++ src/ components/nav_bar/nav_bar.jsx
@@ -33,44 +33,60 @@ const NavBar = (props) => (
       </a>
     </li>
     <li className={styles.item}>
-      <a className={styles.link} title='YouTube Music' href='/'>
+      <a className={styles.link} title='보관함' href='/'>
         <i className='fas fa-archive'></i>
         보관함
       </a>
     </li>
     <li className={styles.item}>
-      <a className={styles.link} title='YouTube Music' href='/'>
+      <a className={styles.link} title='시청 기록' href='/'>
         <i className='fas fa-history'></i>
         시청 기록
       </a>
     </li>
     <li className={styles.item}>
-      <a className={styles.link} title='YouTube Music' href='/'>
+      <a className={styles.link} title='내 동영상' href='/'>
         <i className='fas fa-user-plus'></i>내 동영상
       </a>
     </li>
     <li className={styles.item}>
-      <a className={styles.link} title='YouTube Music' href='/'>
+      <a className={styles.link} title='나중에 볼 동영상' href='/'>
         <i className='fas fa-clock'></i>
         나중에 볼 동영상
       </a>
     </li>
     <li className={styles.item}>
-      <a className={styles.link} title='YouTube Music' href='/'>
+      <a className={styles.link} title='좋아요 표시한 동영상' href='/'>
         <i className='fas fa-thumbs-up'></i>
         좋아요 표시한 동영상
       </a>
     </li>
     <li className={styles.item}>
-      <a className={styles.link} title='YouTube Music' href='/'>
+      <a className={styles.link} title='더보기' href='/'>
         <i className='fas fa-chevron-down'></i>
         더보기
       </a>
     </li>
     <li className={styles.item}>
-      <a className={styles.link} title='BY GitHub' href='https://github.com/ByungyeonKim'>
+      <a
+        className={styles.link}
+        title='BYTube GitHub'
+        href='https://github.com/ByungyeonKim'
+        target='_blank'
+      >
         <i className='fab fa-github'></i>
         BYTube GitHub
+      </a>
+    </li>
+    <li className={styles.item}>
+      <a
+        className={styles.link}
+        title='개발 전용차선 블로그'
+        href='https://onlydev.tistory.com/'
+        target='_blank'
+      >
+        <i className='fas fa-blog'></i>
+        개발 전용차선 블로그
       </a>
     </li>
   </ul>
