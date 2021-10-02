f541db3b50d8be988eaeffcd081cb53d22df80cb
ByungyeonKim <viva_kby@naver.com>
2021-10-02 21:06:22 +0900

    API 응답 nextPageToken 값 추가

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -12,16 +12,17 @@ class Youtube {
         maxResults: '36',
       },
     });
-    return response.data.items.map((item) => {
-      return {
+    return {
+      page: response.data.nextPageToken,
+      items: response.data.items.map((item) => ({
         id: item.id,
         channelId: item.snippet.channelId,
         thumbnailURL: item.snippet.thumbnails.medium.url,
         title: item.snippet.title,
         channelTitle: item.snippet.channelTitle,
         description: item.snippet.description,
-      };
-    });
+      })),
+    };
   }
 
   async search(query) {
@@ -33,28 +34,33 @@ class Youtube {
         type: 'video',
       },
     });
-    return response.data.items.map((item) => ({
-      id: item.id.videoId,
-      channelId: item.snippet.channelId,
-      thumbnailURL: item.snippet.thumbnails.medium.url,
-      title: item.snippet.title,
-      channelTitle: item.snippet.channelTitle,
-      description: item.snippet.description,
-    }));
+    return {
+      page: response.data.nextPageToken,
+      items: response.data.items.map((item) => ({
+        id: item.id.videoId,
+        channelId: item.snippet.channelId,
+        thumbnailURL: item.snippet.thumbnails.medium.url,
+        title: item.snippet.title,
+        channelTitle: item.snippet.channelTitle,
+        description: item.snippet.description,
+      })),
+    };
   }
 
   channel(videos, promises) {
-    for (let i = 0; i < videos.length; i++) {
+    for (let i = 0; i < videos.items.length; i++) {
       const response = this.youtube
         .get('channels', {
           params: {
             part: 'snippet',
             maxResults: '36',
-            id: videos[i].channelId,
+            id: videos.items[i].channelId,
           },
         })
-        .then((result) => result.data.items[0].snippet.thumbnails.default.url)
-        .then((url) => (videos[i].channelThumbnails = url));
+        .then(
+          (result) => result.data.items[0].snippet.thumbnails.default.url
+        )
+        .then((url) => (videos.items[i].channelThumbnails = url));
       promises.push(response);
     }
     return promises;
