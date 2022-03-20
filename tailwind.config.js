675a264eb2302ac3b158e2afd7c8a81bc5e68948
ByungyeonKim <viva_kby@naver.com>
2022-03-20 23:29:38 +0900

    :wrench: tailwind.config 기본색상 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'rgba(33, 33, 33, 0.98)',
+      },
+    },
     screens: {
       sm: '465px',
       md: '768px',
